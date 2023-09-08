b1496677c51c6b8dab6d6e247ed834018fae4a2b
Hector Rojas <hdrojas777@gmail.com>
2023-09-08 10:47:08 -0600

    clase agosto 8 update

--- NODE&NPM/CLASE-AGOSTO-8/index.js
+++ NODE&NPM/CLASE-AGOSTO-8/index.js
@@ -1,17 +1,27 @@
 //importar modulo 
-const http = require('http');
+const http = require('http')
 
 //port: por buena practica debe crearse en una constante indepediente aparte
-const port = 3000;
+const PORT = 8080
 
 //instancia
-const server = http.createServer((req, res) =>{
+const server = http.createServer((req, res) => {
     
+    if (req.method === "GET" && req.url === "/users") {
+        console.log("hola, esta solicitando en la ruta get");
+    }
+    if (req.method === "POST" && req.url === "/users/admins") {
+        console.log("hola, esta solicitando en la ruta post");
+    }
+    if (req.method === "PUT" && req.url === "/users") {
+        console.log("hola, esta solicitando en la ruta put");
+    }
+
 })
 //req: { body , metodo(GET, POST)} / URL / localhost = 127.0.0.1 / users
 //res: http status code / datos / informacion
 
 //listen
-server.listen(port, ()=> {
-console.log(`El servidor esta en el puerto ` + port)
+server.listen(PORT, ()=> {
+console.log(`El servidor esta en el puerto ` + PORT);
 });
