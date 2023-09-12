68dc3a9ec91267ce7064111fc3f1766e05d68bea
Hector Rojas <hdrojas777@gmail.com>
2023-09-12 06:59:16 -0600

    carpetas re ordenadas

--- NODE&NPM/CLASE-AGOSTO-1/promesas.js
+++
@@ -1,9 +0,0 @@
-const ejemplo = () => {
-    return new Promise ((resolve, reject)=>{
-        setTimeout(() =>{
-            resolve('la promesa se cumplio')
-        }, 1000);
-    })
-}
-
-console.log(ejemplo().then(res => clg.log(res)));
--- NODE&NPM/CLASE-AGOSTO-1/promesas2.js
+++
@@ -1,9 +0,0 @@
-const ejemplo = () => {
-    return new Promise ((resolve, reject)=>{
-        setTimeout(() =>{
-            reject('la promesa no se cumplio')
-        }, 1000);
-    })
-}
-
-console.log(ejemplo().then(res => clg.log(res)));
